a215d9f7e2cdbb8301fd6a44eaebc11590ec170c
sergej_99 <sergejmalesevic8@gmail.com>
2024-06-24 22:07:26 +0200

    dummyData

---
+++ data/products.ts
@@ -0,0 +1,40 @@
+const products = [
+    {
+        id:1,
+        name: "Curry 2",
+        price: 125,
+        image:"../../../assets/images/Curry2.jpg"
+    },
+    {
+        id:2,
+        name: "Kobe 6",
+        price: 145,
+        image:"../../../assets/images/Kobe6.jpg"
+    },
+    {
+        id:3,
+        name: "Luka 11",
+        price: 110,
+        image:"../../../assets/images/Luka1.jpg"
+    },
+    {
+        id:4,
+        name: "Lebron XX",
+        price: 175,
+        image:"../../../assets/images/LebronXX.jpg"
+    },
+    {
+        id:5,
+        name: "KD 9",
+        price: 190,
+        image:"../../../assets/images/KD9.jpg"
+    },
+    {
+        id:6,
+        name: "Jordan 4",
+        price: 325,
+        image:"../../../assets/images/Jordan4.jpg"
+    },
+]
+
+export default products;
